a76e09c30456f0e60ff3ff5544c75fe4b8f62e79
Борис Шестериков <boris20033@gmail.com>
2024-02-14 15:27:17 +0400

    Del const arg

--- include/set.h
+++ include/set.h
@@ -1,3 +1,5 @@
+#pragma once
+
 #include <iostream>
 	
 namespace Set {
@@ -21,34 +23,12 @@ namespace Set {
 		Set(const Set& other);
 		~Set();
 
-		void print() const;
-		bool insert(const int key);
-		bool contains(const int key);
-		bool erase(const int key);
+		void print();
+		bool insert(int key);
+		bool contains(int key);
+		bool erase(int key);
 
-		Set& operator+(const Set& set);
+		Set& operator=(const Set& set);
 	};
 
-	Set::Node* Set::find(const int key) {
-		if (root->key == key) return root;
-
-		return nullptr;
-	}
-
-	void Set::print() const {
-		std::cout << key << " ";
-		if (left) left->print();
-		if (right) right->print();
-	}
-
-	bool Set::insert(const int key) {
-		if (find(key)) return false;
-		if (key < this->key)
-			if (left)
-				left->insert(key);
-			else
-				if (key > this->key)
-					if (right)
-						right->insert(key);
-	}
 }
